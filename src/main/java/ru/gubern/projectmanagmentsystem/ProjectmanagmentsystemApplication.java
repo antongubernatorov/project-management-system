0a91f242484ecfcbb5464b9131660e909934ede3
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2024-11-07 20:31:12 +0300

    feat: собран проект

---
+++ src/main/java/ru/gubern/projectmanagmentsystem/ProjectmanagmentsystemApplication.java
@@ -0,0 +1,13 @@
+package ru.gubern.projectmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmanagmentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gubern/projectmanagmentsystem/ProjectmanagmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gubern.projectmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
